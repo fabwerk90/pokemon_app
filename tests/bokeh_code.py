7f7935cfc3129ae86b3f1901494a62ebfc10dd28
Fabian Werkmeister <fabwerkmeister90@gmail.com>
2022-12-08 19:08:25 +0100

    finalized testing

--- tests/bokeh_code.py
+++ tests/bokeh_code.py
@@ -1,10 +1,20 @@
 import pandas as pd
 from bokeh.models import ColumnDataSource
-DATA_PATH = "../data/pokemon_250_stats_and_imageurls.csv"
 
 def read_data_and_build_datasource():
 
-    df = pd.read_csv(DATA_PATH)
+    test_pokemon_dict = {
+        'Name': ["PokemonA", "PokemonB"],
+        'Type 1': ["TypeA", "TypeB"],
+        'Total': [50, 100],
+        'HP': [50, 100],
+        'Attack': [50, 100],
+        'Defense': [50, 100],
+        'Speed': [50, 100],
+        'image_urls': ["URLA", "URLB"]
+         }
+
+    df = pd.DataFrame(data=test_pokemon_dict)
 
     datasource = ColumnDataSource(
     data={
