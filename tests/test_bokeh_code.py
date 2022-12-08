8e3c1bb035c16d2aff873f78426175ecf42d1d59
Fabian Werkmeister <fabwerkmeister90@gmail.com>
2022-12-08 18:57:54 +0100

    updated test functionality

--- tests/test_bokeh_code.py
+++ tests/test_bokeh_code.py
@@ -1,6 +1,7 @@
-from bokeh_code import read_data_and_build_datasource
+import pytest
+import bokeh_code
 
 def test_read_data_and_build_datasource():
 
     correct_bokeh_type = "bokeh.models.sources.ColumnDataSource"
-    assert type(read_data_and_build_datasource()) != correct_bokeh_type
+    assert type(bokeh_code.read_data_and_build_datasource()) != correct_bokeh_type
